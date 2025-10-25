865ac9c2052d67ea68db4ad84cf7d38b45eb0c90
Danithza <158111921+Danithza@users.noreply.github.com>
2025-10-25 17:52:53 -0500

    cambios en app.ts, se agrego cors

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,33 @@
 import express from "express";
 import "reflect-metadata";
+import cors from "cors";
 import { AppDataSource } from "./infrastructure/db/AppDataSource";
 import userRoutes from "./routes";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
 app.use(express.json());
 
-// ✅ Asigna prefijo a todas las rutas de usuario
+
+app.use(
+  cors({
+    origin: "http://127.0.0.1:8080",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], 
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+
 app.use("/api/users", userRoutes);
+
 
 AppDataSource.initialize()
   .then(() => {
     console.log("✅ Database connected");
-    app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+    app.listen(PORT, () =>
+      console.log(`🚀 Server running on http://localhost:${PORT}`)
+    );
   })
   .catch((error) => console.error("❌ Database connection error:", error));
